d49051d8a813e8ded868bff7109779349607e253
Mohamed <s2000588@edu.bc.fi>
2023-11-05 18:40:34 +0200

    add private key to env file

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,19 +1,23 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: [
-    '@nuxtjs/tailwindcss'
-  ],
+  modules: ["@nuxtjs/tailwindcss"],
   app: {
     head: {
-      title: 'My First Nuxt App', 
+      title: "My First Nuxt App",
       meta: [
-        {name: 'viewport', content: 'width=device-width, initial-scale=1'},
-        {name: 'description', content: 'My first Nuxt.js app'}
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
+        { name: "description", content: "My first Nuxt.js app" },
       ],
       link: [
-        {rel: 'stylesheet', href: 'https://fonts.googleapis.com/icon?family=Material+Icons'}
-      ]
-    }
-  }
+        {
+          rel: "stylesheet",
+          href: "https://fonts.googleapis.com/icon?family=Material+Icons",
+        },
+      ],
+    },
+  },
+  runtimeConfig: {
+    currencyKey: process.env.CURRENCY_API_KEY,
+  },
 });
--- pages/about.vue
+++ pages/about.vue
@@ -1,12 +1,12 @@
 <template>
   <div>
     <h2>About</h2>
-    <p v-for="d in data">{{ d.code }}</p>
+    <p>Current currency: {{ data }}</p>
   </div>
 </template>
 
 <script setup>
-const { data } = await useFetch("/api/test");
+const { data } = await useFetch("/api/currency/GBP");
 </script>
 
 <style scoped></style>
