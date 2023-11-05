d997a41fb717bc36c81590d69cfeab02c9f3eb58
Mohamed <s2000588@edu.bc.fi>
2023-11-05 15:44:13 +0200

    create reuseable card component

--- pages/products/index.vue
+++ pages/products/index.vue
@@ -2,7 +2,7 @@
   <div>
     <div class="grid grid-cols-4 gap-5">
       <div v-for="p in products">
-          <NuxtLink :to="`/products/${p.id}`">{{p.title}}</NuxtLink>
+        <ProductCard :product="p"/>
       </div>
     </div>
   </div>
@@ -12,7 +12,7 @@
 definePageMeta({
   layout: "products",
 });
-const {data: products} = await useFetch('https://fakestoreapi.com/products')
+const { data: products } = await useFetch("https://fakestoreapi.com/products");
 console.log(products._rawValue);
 </script>
 
