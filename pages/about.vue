bcb7e1255d8f717538b7c3aa5d7efed9c2a0e984
Mohamed <s2000588@edu.bc.fi>
2023-11-05 10:38:59 +0200

    delete app.vue and create pages folder

---
+++ pages/about.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h2>About</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam nihil dolorem vero eaque provident doloribus, nam ad, incidunt aut distinctio quos, atque aperiam sapiente quo dolores recusandae explicabo libero est!</p>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
