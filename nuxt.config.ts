30070af6057974e3c542bc52cbc863af811c38f6
Mohamed <s2000588@edu.bc.fi>
2023-11-05 16:24:52 +0200

    add metadata for all pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,4 +4,16 @@ export default defineNuxtConfig({
   modules: [
     '@nuxtjs/tailwindcss'
   ],
+  app: {
+    head: {
+      title: 'My First Nuxt App', 
+      meta: [
+        {name: 'viewport', content: 'width=device-width, initial-scale=1'},
+        {name: 'description', content: 'My first Nuxt.js app'}
+      ],
+      link: [
+        {rel: 'stylesheet', href: 'https://fonts.googleapis.com/icon?family=Material+Icons'}
+      ]
+    }
+  }
 });
