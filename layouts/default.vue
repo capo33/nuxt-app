255eab2a1bc795d451951213b6a0e442af2ed62c
Mohamed <s2000588@edu.bc.fi>
2023-11-05 15:07:47 +0200

    Fix formatting of Home link in navigation

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,7 +4,7 @@
       <nav class="container mx-auto p-4 flex justify-between">
         <NuxtLink to="/" class="font-bold">Nuxt App</NuxtLink>
         <ul class="flex gap-4">
-          <li><NuxtLink to="/">Home</NuxtLink></li>
+          <li><NuxtLink to="/" >Home</NuxtLink></li>
           <li><NuxtLink to="/about">About</NuxtLink></li>
           <li><NuxtLink to="/products" class="btn">Products</NuxtLink></li>
         </ul>
